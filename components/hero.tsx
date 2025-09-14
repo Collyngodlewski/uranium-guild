43e7b85875370c57915b822b264c9e10611c2ae6
Collyn Godlewski <collyngodlewski@gmail.com>
2025-09-14 16:28:18 -0400

    change to banner

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import uraniumBanner from '../app/uranium_banner.jpg'
+import uraniumBanner from '../app/Toxic_Power__Uranium_Hazard_Logo.png'
 
 export function Hero() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-        
+    <div className="flex flex-col gap-16 items-center w-full  ">
+        <div className="uranium-banner w-full flex justify-center">
             <Image
               src={uraniumBanner}
-              width={2550}
-              height={2550}
+              width={200}
+              height={200}
               alt="Uranium Banner"
-              // className="w-full"
+              className=""
             />
-       
+       </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
