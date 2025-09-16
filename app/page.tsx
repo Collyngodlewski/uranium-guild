36daef34bedb68693d40bb4b7c30c52fcaa236fd
Collyn Godlewski <collyngodlewski@gmail.com>
2025-09-15 21:25:07 -0400

    pictures!!!!

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
-
+import { HomepageBackgroundImage } from "@/components/homepage-background";
 
 export default function Home() {
   return (
@@ -20,10 +20,15 @@ export default function Home() {
           </div>
         </nav>
           <Hero />
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Mythic Guild on Stormrage-US</h1>
-        <p className="mb-10 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">Raids Tuesday & Wednesday from 8pm to 11pm EST.</p>     
+        <div className=" flex flex-col items-center gap-28 w-full">
+         
+          <div>
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Mythic Guild on Stormrage-US</h1>
+            <p className="mb-10 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">Raids Tuesday & Wednesday from 8pm to 11pm EST.</p>
+            <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
+          </div> 
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "2rem", width: "100%", justifyContent: "center" }}>
-          <div style={{ width: 500, height: 228, overflow: "hidden", maxWidth: "100%" }}>
+          <div style={{  overflow: "hidden", maxWidth: "100%" }}>
             <iframe
               src="https://raider.io/widgets/boss-progress?raid=latest&name_style=logo&difficulty=latest&region=us&realm=stormrage&guild=Uranium&boss=latest&period=until_kill&orientation=rect&hide=&chromargb=transparent&theme=dragonflight"
               width={500}
@@ -39,26 +44,29 @@ export default function Home() {
               allowTransparency={true}
             />
           </div>
-          <div>
-            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-white">CE Liberation of Undermine US Rank 476</p>
-            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-white">CE Nerubar Palace US Rank 606</p>
-            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-white">7/9M Amirdrassil, the Dream's Hope</p>
-            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-white">5/9M Aberrus, the Shadowed Crucible</p>
-            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-white">5/8M Vault of the Incarnates</p>
+          <div className="w-[500px] text-center">
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl  dark:text-white">CE Liberation of Undermine US Rank 476</p>
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl  dark:text-white">CE Nerubar Palace US Rank 606</p>
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl  dark:text-white">7/9M Amirdrassil, the Dream's Hope</p>
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl  dark:text-white">5/9M Aberrus, the Shadowed Crucible</p>
+            <p className="mb-6 text-lg font-normal text-gray-900 lg:text-xl  dark:text-white">5/8M Vault of the Incarnates</p>
           </div>
         </div>
-        <Link
-          href="/apply"
-          className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-green-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto mb-16"
-        >
-          Apply Now!
-          <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-          </svg>
-        </Link>
-          
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+          <Link
+            href="/apply"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mx-auto mb-16"
+          >
+            Apply Now!
+            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </Link>
+              <div className="homepage-image">
+            <HomepageBackgroundImage />
+          </div>
+        </div>
+  
+        <footer className="w-full bg-[background] flex items-center justify-center border-t mx-auto mt-auto text-center text-xs gap-8 py-8">
           <p>Made with Love by{" "}
             <a
               href="https://worldofwarcraft.blizzard.com/en-us/character/us/stormrage/cheery"
